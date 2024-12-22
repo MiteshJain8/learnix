2dcb82b1a90f015927d8a9dbcb8c8d597002bdab
Haksham <harsh924hashvm@gmail.com>
2024-12-22 16:51:13 +0530

    weather invalid

--- app/components/WeatherApp.js
+++ app/components/WeatherApp.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import FinalInputCard from "../components/inputCard/FinalInputCard";
 import FinalOutput from "../components/outputCard/FinalOutput";
+import { useRouter } from "next/navigation";
 
 const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const router = useRouter();
 
   const handleSearch = async (city) => {
     const API_KEY = "c4b469446da64a62a7455412241903";
@@ -11,8 +13,13 @@ const WeatherApp = () => {
       const response = await fetch(
         `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=yes`
       );
+      if (!response.ok) {
+        alert("Invalid city name");
+        router.push("/");
+        
+      }else{
       const data = await response.json();
-      setWeatherData(data);
+      setWeatherData(data);}
     } catch (error) {
       console.error("Error fetching the weather data: ", error);
     }
